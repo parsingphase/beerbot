2871565c9b0a524dc1810fbad1bd96cf4308fc80
Richard George <richard@phase.org>
2018-10-16 19:01:53 +0100

    beerByDay: add weekly gather

--- beerByDay.py
+++ beerByDay.py
@@ -5,6 +5,7 @@ import re
 import sys
 import json
 from typing import Optional
+from datetime import timedelta
 from dateutil.parser import parse as parse_date  # pipenv install  python-dateutil
 
 DEFAULT_UNIT = 'pint'
@@ -104,10 +105,10 @@ for checkin in source_data:
     # fields of interest: comment, created_at, beer_abv, serving_type
     abv = float(checkin['beer_abv'])
     created_at = parse_date(checkin['created_at'])  # <class 'datetime.datetime'>
-    date = created_at.date().isoformat()
+    date_key = created_at.date().isoformat()
 
-    if date not in daily:
-        daily[date] = {
+    if date_key not in daily:
+        daily[date_key] = {
             'drinks': 0,
             'units': 0,
             'alcohol_ml': 0,
@@ -115,36 +116,76 @@ for checkin in source_data:
             'estimated': ''
         }
 
-    daily[date]['drinks'] += 1
+    daily[date_key]['drinks'] += 1
 
     measure = measure_from_comment(checkin['comment'])
     if measure is None:
         measure = measure_from_serving(checkin['serving_type'])
-        daily[date]['estimated'] = '*'
+        daily[date_key]['estimated'] = '*'
 
     if measure:
-        daily[date]['beverage_ml'] += measure
+        daily[date_key]['beverage_ml'] += measure
         if abv:
             alcohol_volume = float(measure) * abv / 100
-            daily[date]['alcohol_ml'] += alcohol_volume
-            daily[date]['units'] += alcohol_volume / 10
+            daily[date_key]['alcohol_ml'] += alcohol_volume
+            daily[date_key]['units'] += alcohol_volume / 10
 
     else:
-        daily[date]['estimated'] = '**'
+        daily[date_key]['estimated'] = '**'
 
-# round numerics to 1dp
-for date in daily:
-    for k in daily[date]:
-        if k != 'estimated':
-            daily[date][k] = round(daily[date][k], 1)
+# round numerics to 1dp, gather weeks
+weekly = {}
+for date_key in daily:
+
+    # calculate week
+    date = parse_date(date_key)
+    iso_calendar = date.isocalendar()  # Y - W - dow
+    week_key = '%d-W%02d' % iso_calendar[0:2]
+    days_since_monday = iso_calendar[2] - 1
+    monday = date - timedelta(days=days_since_monday)
+    if week_key not in weekly:
+        weekly[week_key] = {
+            'week': week_key,
+            'commencing': monday.date().isoformat(),
+            'drinks': 0,
+            'units': 0,
+            'alcohol_ml': 0,
+            'beverage_ml': 0,
+            'estimated': ''
+        }
+
+    for k in daily[date_key]:
+        if k == 'estimated':
+            if len(daily[date_key][k]) > len(weekly[week_key][k]):
+                weekly[week_key][k] = daily[date_key][k]
+
+        else:
+            weekly[week_key][k] += daily[date_key][k]
+            daily[date_key][k] = round(daily[date_key][k], 1)
 
 row = ['date'] + keys
 writer.writerow(row)
 
-for date in daily:
-    row = [date]
+for date_key in daily:
+    row = [date_key]
     for k in keys:
-        row.append(daily[date][k])
+        row.append(daily[date_key][k])
+
+    writer.writerow(row)
+
+for i in range(5):
+    writer.writerow([''])
+
+row = ['week', 'commencing'] + keys
+writer.writerow(row)
+
+for week_key in weekly:
+    row = [week_key, weekly[week_key]['commencing']]
+    for k in keys:
+        cell_value = weekly[week_key][k]
+        if k != 'estimated':
+            cell_value = round(cell_value, 1)
+        row.append(cell_value)
 
     writer.writerow(row)
 
