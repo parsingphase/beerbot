1b04376221cc50ef9ccc7cb49b85b6d2cebf8ebc
Richard George <richard@phase.org>
2018-10-18 14:32:44 +0100

    Move code in stockCheck.py to functions

--- stockCheck.py
+++ stockCheck.py
@@ -15,94 +15,97 @@ def file_contents(file_path: str) -> Optional[str]:
     return ''.join(contents)
 
 
-parser = argparse.ArgumentParser(
-    description='Summarise expiry dates and types of beers on a list',
-    usage=sys.argv[0] + ' SOURCE [--output OUTPUT] [--help]'
-)
-parser.add_argument('source', help='Path to source file (export.json)')
-parser.add_argument('--output', required=False, help='Path to output file, STDOUT if not specified')
-args = parser.parse_args()
-
-source = args.source
-dest = args.output
-
-source_data = json.loads(file_contents(source))
-
-source_data.sort(key=lambda b: b['best_by_date_iso'])
-
-thresholds = {
-    'undated': {'description': 'Undated beers', 'ends': '0000-00-00'},
-    'now': {'description': 'Expired beers', 'ends': date.today().strftime('%Y-%m-%d')},
-    'month': {'description': 'Within one month',
-              'ends': (date.today() + relativedelta(months=+1)).strftime('%Y-%m-%d')},
-    'two': {'description': 'Within two months', 'ends': (date.today() + relativedelta(months=+2)).strftime('%Y-%m-%d')},
-    'future': {'description': 'More than two months away'}
-}
-
-slices = {'undated': [], 'now': [], 'month': [], 'two': [], 'future': []}
-styles = {}
-
-for item in source_data:
-    style = item['beer_type'].split(' -')[0].strip()
-    if not style in styles:
-        styles[style] = 0
-    styles[style] += int(item['quantity'])
-
-    due = item['best_by_date_iso']
-    if due <= thresholds['undated']['ends']:
-        slices['undated'].append(item)
-    elif due < thresholds['now']['ends']:
-        slices['now'].append(item)
-    elif due < thresholds['month']['ends']:
-        slices['month'].append(item)
-    elif due < thresholds['two']['ends']:
-        slices['two'].append(item)
-    else:
-        slices['future'].append(item)
-
-style_list = []
-for style in styles:
-    style_list.append({'style': style, 'count': styles[style]})
-
-style_list.sort(key=lambda b: (0 - b['count'], b['style']))
-
-if dest:
-    output_handle = open(dest, 'w')
-else:
-    output_handle = sys.stdout
-
-writer = csv.writer(output_handle)
-
-for k in slices:
-    writer.writerow(
-        [
-            '%s: %d item(s) of %d beer(s)' % (
-                thresholds[k]['description'], sum([int(s['quantity']) for s in slices[k]]), len(slices[k]),
-            )
-        ]
+def parse_cli_args():
+    parser = argparse.ArgumentParser(
+        description='Summarise expiry dates and types of beers on a list',
+        usage=sys.argv[0] + ' SOURCE [--output OUTPUT] [--help]'
     )
-    if len(slices[k]) == 0:
-        writer.writerow(['(NONE)'])
+    parser.add_argument('source', help='Path to source file (export.json)')
+    parser.add_argument('--output', required=False, help='Path to output file, STDOUT if not specified')
+    args = parser.parse_args()
+    return args
+
+
+def build_dated_list_summary(source_data, output_handle):
+    source_data.sort(key=lambda b: b['best_by_date_iso'])
+    thresholds = {
+        'undated': {'description': 'Undated beers', 'ends': '0000-00-00'},
+        'now': {'description': 'Expired beers', 'ends': date.today().strftime('%Y-%m-%d')},
+        'month': {'description': 'Within one month',
+                  'ends': (date.today() + relativedelta(months=+1)).strftime('%Y-%m-%d')},
+        'two': {'description': 'Within two months',
+                'ends': (date.today() + relativedelta(months=+2)).strftime('%Y-%m-%d')},
+        'future': {'description': 'More than two months away'}
+    }
+    slices = {'undated': [], 'now': [], 'month': [], 'two': [], 'future': []}
+    styles = {}
+    for item in source_data:
+        style = item['beer_type'].split(' -')[0].strip()
+        if not style in styles:
+            styles[style] = 0
+        styles[style] += int(item['quantity'])
+
+        due = item['best_by_date_iso']
+        if due <= thresholds['undated']['ends']:
+            slices['undated'].append(item)
+        elif due < thresholds['now']['ends']:
+            slices['now'].append(item)
+        elif due < thresholds['month']['ends']:
+            slices['month'].append(item)
+        elif due < thresholds['two']['ends']:
+            slices['two'].append(item)
+        else:
+            slices['future'].append(item)
+    style_list = []
+    for style in styles:
+        style_list.append({'style': style, 'count': styles[style]})
+    style_list.sort(key=lambda b: (0 - b['count'], b['style']))
+
+    writer = csv.writer(output_handle)
+    for k in slices:
+        writer.writerow(
+            [
+                '%s: %d item(s) of %d beer(s)' % (
+                    thresholds[k]['description'], sum([int(s['quantity']) for s in slices[k]]), len(slices[k]),
+                )
+            ]
+        )
+        if len(slices[k]) == 0:
+            writer.writerow(['(NONE)'])
+        else:
+            for item in slices[k]:
+                writer.writerow(
+                    [
+                        item['best_by_date_iso'],
+                        item['quantity'],
+                        item['brewery_name'],
+                        item['beer_name'],
+                        item['beer_type'],
+                        '%.1f%%' % float(item['beer_abv']),
+                        item['container'],
+                    ]
+                )
+    for i in range(5):
+        writer.writerow([''])
+    writer.writerow(['Styles'])
+    for style_row in style_list:
+        writer.writerow([style_row['style'], style_row['count']])
+    if dest:
+        output_handle.close()
+
+
+def run_cli():
+    global dest
+    args = parse_cli_args()
+    source = args.source
+    dest = args.output
+    if dest:
+        output_handle = open(dest, 'w')
     else:
-        for item in slices[k]:
-            writer.writerow(
-                [
-                    item['best_by_date_iso'],
-                    item['quantity'],
-                    item['brewery_name'],
-                    item['beer_name'],
-                    item['beer_type'],
-                    '%.1f%%' % float(item['beer_abv']),
-                    item['container'],
-                ]
-            )
+        output_handle = sys.stdout
+    source_data = json.loads(file_contents(source))
+    build_dated_list_summary(source_data, output_handle)
 
-for i in range(5):
-    writer.writerow([''])
 
-writer.writerow(['Styles'])
-for style_row in style_list:
-    writer.writerow([style_row['style'], style_row['count']])
-
-if dest:
-    output_handle.close()
+if __name__ == '__main__':
+    run_cli()
