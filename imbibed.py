704fa9329393d8dd4b5acb1c6b2ce58d5a2999be
Richard George <richard@phase.org>
2019-01-17 20:42:55 +0000

    Add 'cl' as a unit, make 'taste/sip' non-zero

--- imbibed.py
+++ imbibed.py
@@ -27,7 +27,7 @@ def parse_measure(measure_string: str) -> int:
     """
     divisors = {'quarter': 4, 'third': 3, 'half': 2}
     divisor_match = '(?P<divisor_text>' + '|'.join(divisors.keys()) + ')'
-    units = {'ml': 1, 'litre': 1000, 'liter': 1000, 'pint': 568, 'sip': 0, 'taste': 0}
+    units = {'ml': 1, 'cl': 10, 'litre': 1000, 'liter': 1000, 'pint': 568, 'sip': 25, 'taste': 25}
     unit_match = '(?P<unit>' + '|'.join(units.keys()) + ')s?'  # allow plurals
     optional_unit_match = '(' + unit_match + ')?'
     fraction_match = '(?P<fraction>\d+/\d+)'
